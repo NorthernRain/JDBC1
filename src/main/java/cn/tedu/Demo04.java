1d4ccbf78d91cd301ccfb0e252dd394fe9c50ca4
NorthernRain <1218680620@qq.com>
2019-09-09 17:29:36 +0800

    delete a extra git repository

--- src/main/java/cn/tedu/Demo04.java
+++ src/main/java/cn/tedu/Demo04.java
@@ -4,6 +4,7 @@ package cn.tedu;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
@@ -25,16 +26,42 @@ public class Demo04 {
 
     @Test
     public void update() {
+        try (Connection connection = DBUtils.getConn()) {
+            Statement statement = connection.createStatement();
+            String sql = "update emp set ename='hanmeimei' where ename='Lucy'";
+            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("update");
     }
 
     @Test
     public void delete() {
+        try (Connection connection = DBUtils.getConn()) {
+            Statement statement = connection.createStatement();
+            String sql = "delete from emp where ename='hanmeimei'";
+            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("delete");
     }
 
     @Test
     public void select() {
+        try (Connection connection = DBUtils.getConn()) {
+            Statement statement = connection.createStatement();
+            String sql = "select empno,ename from emp where ename='hanmeimei'";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int empno = resultSet.getInt(1);
+                String ename = resultSet.getString(2);
+                System.out.println(empno + ":" + ename);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("select");
     }
 }
